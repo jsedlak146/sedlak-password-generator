d606ddaf61f9d83691b3c2ec6992ad1c73cd985e
jsedlak146 <jssedlak146@gmail.com>
2022-11-27 17:45:39 -0500

    java fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // Assignment Code
 
-
 //prompt user for password criteria
 
 //length of password has to be at least 8 character
@@ -8,91 +7,112 @@
 
 //password needs to include lc,uc,numeric or special characters
 
-//var uppercase = ['A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'].split(' , ')
-//var lowercase = ['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'].split(' , ')
-//var specChar = ['!,@,#,$,%,^,&,*'].split(' , ')
-//var numbers = ['1,2,3,4,5,6,7,8,9'].split(' , ')
+function getPasswordOptions() {
+  const length = parseInt(
+    prompt("How many characters would you like in your password?")
+  );
+  console.log(length);
 
-function passEnter () {
-    var length = parseInt (
-      prompt('How many characters would you like in your password?')
-    );
-    console.log(length)
-//makes sure user puts in numeric value
-
-    if (Number.isNaN(length)) {
-    alert('password must be betwen 8 and 128 characters');
+  //makes sure user puts in numeric value
+  if (Number.isNaN(length)) {
+    alert("password must be betwen 8 and 128 characters");
     return null;
+  }
+
+  //conditional to keep it between 8-128 characters
+  if (length < 8 || length > 128) {
+    alert("Please keep your password from 8 and 128 characters");
+    return null;
+  }
+  //conditional statement to check what characters wants user to have, confirm takes in true/false
+  const hasLowerCase = confirm("Do you want to incorporate lowecase letter?");
+  const hasUpperCase = confirm("Do you want to incorporate uppercase letters?");
+  const hasSpecChar = confirm("Do you want to use special characters?");
+  const hasNumbers = confirm("Do you want to use numbers?");
+
+  //create conditional for one character type
+  if (
+    hasLowerCase === false &&
+    hasUpperCase === false &&
+    hasSpecChar === false &&
+    hasNumbers === false
+  ) {
+    alert("Must choose one char type");
+    return null;
+  }
+
+  //keys
+  const passwordOptions = {
+    userLength: length,
+    wantLowerCase: hasLowerCase,
+    wantUpperCase: hasUpperCase,
+    wantSpecChar: hasSpecChar,
+    wantNumbers: hasNumbers,
+  };
+  return passwordOptions;
 }
-passEnter()
 
-//conditional to keep it between 8-128 characters
+function getPasswordValue(passwordOptions) {
+  const uppercase = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(
+    ","
+  );
+  const lowercase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(
+    ","
+  );
+  const specChar = "!,@,#,$,%,^,&,*".split(",");
+  const numbers = "0,1,2,3,4,5,6,7,8,9".split(",");
+  const choices = [];
 
-if (length < 8) {
-  alert('Please keep your password from 8 and 128 characters');
-  return null;
+  // if we do not have any options, return
+  if (!passwordOptions) return null;
+
+  //conditional for addition of lowercase/uppercase to possibilities
+  if (passwordOptions.wantLowerCase) {
+    choices.push(lowercase);
+  }
+  if (passwordOptions.wantUpperCase) {
+    choices.push(uppercase);
+  }
+  if (passwordOptions.wantSpecChar) {
+    choices.push(specChar);
+  }
+  if (passwordOptions.wantNumbers) {
+    choices.push(numbers);
+  }
+
+  //Loop through our array of choices and get random characters from each
+  let password = "";
+  for (let i = 0; i < passwordOptions.userLength; i++) {
+    const c = i % choices.length;
+    const choice = choices[c];
+    const character = getRandomInt(choice.length);
+    password += choice[character];
+  }
+  return password;
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
-//conditional to keep it below 128 charcters
-
-if (length > 128) {
-  alert('Please keep your password from 8 and 128 characters');
-  return null;
+///make a function that randomizes characters
+function generatePassword() {
+  const options = getPasswordOptions();
+  console.log(options);
+  if (options) {
+    return getPasswordValue(options);
+  }
 }
 
-//conditional statement to check what characters wants user to have
-
-var haslowerCase = confirm('Do you want to incorporate lowecase letter?');
-var hasUppercase = confirm('Do you want to incorporate uppercase letters?');
-var hasspecChar = confirm('Do you want to use special characters?');
-var hasNumbers = confirm('Do you want to use numbers?');
-
-//create conditional for one character type
-
-if (
-haslowerCase === false &&
-hasUppercase === false &&
-hasspecChar === false &&
-hasNumbers === false 
-) {
-  alert('Must choose one char type')
-  return null;
-}
-
-//keys
-
-var passwordOptions = {
-  length: length,
-  haslowerCase: haslowerCase,
-  hasUppercase: hasUppercase,
-  hasspecChar: hasspecChar,
-  hasNumbers: hasNumbers,
-
-};
-
-console.log(passwordOptions)
-return passwordOptions
-
-
-}
-passEnter()
-
-
-
-
-
-
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  const password = generatePassword();
+  if (password) {
+    const passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
